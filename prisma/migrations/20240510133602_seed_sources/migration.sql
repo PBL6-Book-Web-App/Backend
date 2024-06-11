467239e72afe5355f964b449aafc37efccd920d1
jasmineChamCham <jasminebkdn@gmail.com>
2024-06-11 18:23:59 +0700

    fix: fix source goodreads

--- prisma/migrations/20240510133602_seed_sources/migration.sql
+++ prisma/migrations/20240510133602_seed_sources/migration.sql
@@ -1,2 +1,2 @@
 INSERT INTO "source" ("id", "name")
-VALUES (1, 'Thrift Books'), (2, 'PBL7_WEB'), (3, 'TIKI'), (4, 'Book Crossing')
+VALUES (1, 'Thrift Books'), (2, 'PBL7_WEB'), (3, 'GoodReads'), (4, 'Book Crossing')
